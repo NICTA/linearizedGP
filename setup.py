471dae114c1598acce13ecefc6a9a355604da790
Daniel Steinberg <dan.m.steinberg@gmail.com>
2014-11-14 16:03:39 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-""" Setup utility for the nonlinGP package. """
+""" Setup utility for the linearizedGP package. """
 
 from distutils.core import setup
 
 setup(
-    name='nonlinGP',
+    name='linearizedGP',
     version='1.0',
     description='Implementation of the extended and unscented Gaussian '
                 'processes.',
     author='Daniel Steinberg',
     author_email='daniel.steinberg@nicta.com.au',
     url='',
-    packages=['nonlinGP'],
+    packages=['linearizedGP'],
     install_requires=[
         "scipy >= 0.12.0",  # These may be able to be lower, just not tested
         "numpy >= 1.8.0",
